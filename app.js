6fb737d760cc23a67b2d23e82aee9469ab78c92c
Dylan <dachaconp@unal.edu.co>
2020-09-03 11:47:58 -0500

    passport

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var usersRouter = require('./routes/users');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 const mongoose = require('mongoose');
-const Dishes = require('./models/dishes');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -39,26 +40,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
