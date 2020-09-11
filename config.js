6c8ff3c070d157bf7fd985f14b8577513aa7ab6c
Dylan <dachaconp@unal.edu.co>
2020-09-11 10:33:20 -0500

    Oauth 2 - facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '317364202688635',
+        clientSecret: '024f216c2461667669c63d364ded1f7f'
+    }
 }
